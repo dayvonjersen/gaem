9590674577a60da446a69e32f92fd98cbc88caa5
tso <tso@teknik.io>
2015-12-21 18:26:24 -0500

    ._.

--- hello.c
+++ hello.c
@@ -1,40 +1,41 @@
 #include <nes.h>
 
+#define write_addr(pointer, value) *((unsigned char*)pointer) = value
+
 void write_string(char *str) {
-    *((unsigned char*)0x2006) = 0x21;
-    *((unsigned char*)0x2006) = 0x42;
+    write_addr(0x2006, 0x21);
+    write_addr(0x2006, 0x42);
 
     while(*str) {
-        *((unsigned char*)0x2007) = *str;
+        write_addr(0x2007, *str);
         str++;
     }
 }
 
 int main(void) {
     int i;
+    long t;
     waitvblank();
 
-    *((unsigned char*)0x2006) = 0x3F;
-    *((unsigned char*)0x2006) = 0x00;
-    *((unsigned char*)0x2007) = 0x01;
-
-    *((unsigned char*)0x2006) = 0x3F;
-    *((unsigned char*)0x2006) = 0x03;
-    *((unsigned char*)0x2007) = 0x30;
-
-    write_string("Hello, NES");
-
-    *((unsigned char*)0x2005) = 0x00;
-    *((unsigned char*)0x2005) = 0x00;
-    *((unsigned char*)0x2001) = 0x08;
-
     i = 0;
+    t = 0;
     for(;;) {
-        *((unsigned char*)0x2006) = 0x3F;
-        *((unsigned char*)0x2006) = 0x00;
-        *((unsigned char*)0x2007) = i;
-        i++;
-        if (i > 255) i = 0;
+        t++;
+        if(t > 350) {
+            write_addr(0x2006, 0x3F);
+            write_addr(0x2006, 0x00);
+            write_addr(0x2007, i);
+
+            i++;
+            if(i > 255) i = 0;
+
+            write_string("._.");
+            write_addr(0x2005, 0x00);
+            write_addr(0x2005, 0x00);
+            write_addr(0x2001, 0x02|0x04|0x08);
+
+            t = 0;
+        }
     }
 
     return 0;
