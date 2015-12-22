8880e755dcd578c2678bfdc37cfe2bddc071a68f
tso <tso@teknik.io>
2015-12-22 17:12:17 -0500

    regress--

--- hello.c
+++ hello.c
@@ -1,41 +1,39 @@
 #include <nes.h>
+#include <stdio.h>
 
-#define write_addr(pointer, value) *((unsigned char*)pointer) = value
+#define read_addr(pointer) *((unsigned char*)pointer)
+#define write_addr(pointer, value) read_addr(pointer) = value
 
 void write_string(char *str) {
     write_addr(0x2006, 0x21);
     write_addr(0x2006, 0x42);
 
-    while(*str) {
+    while(*str != '\0') {
         write_addr(0x2007, *str);
         str++;
     }
 }
 
 int main(void) {
-    int i;
-    long t;
+    char text[12];
+
     waitvblank();
 
-    i = 0;
-    t = 0;
+    write_addr(0x2006, 0x3F);
+    write_addr(0x2006, 0x00);
+    write_addr(0x2007, COLOR_VIOLET);
+
+    write_addr(0x2006, 0x3F);
+    write_addr(0x2006, 0x03);
+    write_addr(0x2007, 0x30);
+
+    sprintf(text, "just a test");
+    write_string(text);
+
+    write_addr(0x2005, 0x00);
+    write_addr(0x2005, 0x00);
+    write_addr(0x2001, 0x08);
     for(;;) {
-        t++;
-        if(t > 350) {
-            write_addr(0x2006, 0x3F);
-            write_addr(0x2006, 0x00);
-            write_addr(0x2007, i);
-
-            i++;
-            if(i > 255) i = 0;
-
-            write_string("._.");
-            write_addr(0x2005, 0x00);
-            write_addr(0x2005, 0x00);
-            write_addr(0x2001, 0x02|0x04|0x08);
-
-            t = 0;
-        }
     }
 
     return 0;
