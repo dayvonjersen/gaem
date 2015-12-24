72fa6c8837d92b8851bcd46ec42d3740b4fed18d
tso <tso@teknik.io>
2015-12-24 05:40:09 -0500

    keep on trying till you run out of cake

--- hello.c
+++ hello.c
@@ -15,21 +15,59 @@ unsigned char read_joystick1() {
     return joy_state;
 }
 
+void delay(int i) {
+    while(i--) {
+        waitvblank();
+    }
+}
+
 int main(void) {
-    unsigned char pad, bgcolor;
-
+    char j, k, l, oy;
+    unsigned int i;
+    unsigned char pad, bgcolor, x, y;
+    const char s[][33] = {
+        " this was a triumph\0", 
+        " i'm making a note here\0", 
+        " HUGE SUCCESS\0",
+        "it's hard to overstate my satis-\0"}; 
     waitvblank();
+    screensize(&x,&y);
 
-    write_addr(0x2006, 0x3F);
-    write_addr(0x2006, 0x00);
-    write_addr(0x2007, COLOR_VIOLET);
+    textcolor(COLOR_WHITE);
 
-    cprintf(" push buttan");
+    clrscr();
+
+    j = y - 4;
+    oy = j;
+
+    cprintf(" push buttan\r\n");
+
+    for(i = 0; i < 256; i++) {
+        cprintf("%c", (char)i);
+        if((i % 30)==0) {
+            cprintf("\r\n");
+        }
+    }
 
     write_addr(0x2005, 0x00);
     write_addr(0x2005, 0x00);
     write_addr(0x2001, 0x08);
     for(;;) {
+        delay(50);
+        for(k = 0; k < 4; k++) {
+            gotoxy(0, oy + k);
+            for(l = 0; l < x; l++) {
+                cputc(32);
+            }
+        }
+        if(--j < 4) {
+            j = y - 4;
+        }
+        for(k = 0; k < 4; k++) {
+            gotoxy(0, j + k);
+            cputs(s[k]);
+        }
+        oy = j;
         pad = read_joystick1();
         if(pad == KEY_UP) {
             bgcolor = COLOR_RED;
