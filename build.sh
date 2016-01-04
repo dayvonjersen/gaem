1de0a453f0a1a88823d033df58100f1aa8e40a0d
tso <tso@teknik.io>
2016-01-04 14:59:23 -0500

    k

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-../cc65/bin/cl65 -L ../cc65/lib -t nes -I ../cc65/include hello.c -o hello.nes
+#../cc65/bin/cl65 -L ../cc65/lib -t nes -I ../cc65/include hello.c -o hello.nes
+../cc65/bin/cc65 --add-source -t nes -I ../cc65/include hello.c
+cat hello.s chrtest.s > temp.s
+mv temp.s hello.s
+../cc65/bin/ca65 hello.s
+../cc65/bin/ld65 -L ../cc65/lib -t nes hello.o ../cc65/lib/nes.lib -o hello.nes
